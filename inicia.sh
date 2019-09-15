812ffc859efdd34727ca102cbb5046aa36f4f55f
evertonmessias <everton.messias@gmail.com>
2019-09-15 13:44:32 -0300

    15-09-2019_13:44:31

--- inicia.sh
+++ inicia.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-cp bkp.env .env
+clear
+function inicia_laravel() 
+{
 cp bkp.gitignore .gitignore
 php artisan serve &
+}
+
+echo "INICIANDO O LARAVEL"
+echo -n "Escolha (m)ysql ou (s)qlite : ";read op
+
+case $op in
+m) cp bkp.mysql.env .env;inicia_laravel;;
+s) cp bkp.sqlite.env .env;inicia_laravel;;
+*) echo "OPCAO INVALIDA";;
+esac
